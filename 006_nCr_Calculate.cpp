123dcd4e32d1176c2881dac0237ab7ee49957daf
Dhruv Bansal <dhruvbansalup@gmail.com>
2024-09-21 17:59:08 +0530

    Rename few files and add file 006

---
+++ 006_nCr_Calculate.cpp
@@ -0,0 +1,32 @@
+// Program to calculate nCr
+
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+    return fact;
+}
+
+int nCr(int n, int r)
+{
+    int numerator = factorial(n);
+    int denominator = factorial(r) / factorial(n - r);
+    int ans=numerator / denominator;
+    return ans;
+}
+
+int main()
+{
+    int n, r;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << "Enter r: ";
+    cin >> r;
+    cout << "nCr: " << nCr(n, r);
+}
